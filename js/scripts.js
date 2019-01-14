3e79aa2dbf6ff8eb206eda4203b9b3eb1d36f3bf
Dino Nguyen and Matt Groberg <dino@>
2019-01-14 10:35:04 -0800

    add other math functions

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,20 @@
 function add(number1, number2) {
-return number1 + number2;
-}
+  return number1 + number2;
+};
 
 var number1 = parseInt(prompt("Enter a number:"));
 var number2 = parseInt(prompt("Enter another number:"));
+var result = add(number1, number2);
+alert(result);
 
-alert(add(number1, number2));
+function subtract(number1, number2) {
+  return number1 - number2;
+}
+
+function multiply(number1, number2) {
+  return number1 * number2;
+}
+
+function divide(number1, number2) {
+  return number1 / number2;
+}
