2131a242d0f0770d37e5805383b53eb0b075ed8c
Ian Cook Westgate and Matt Groberg <matt@>
2019-01-16 11:16:46 -0800

    Add other arithmetical functions and forms.

--- js/scripts.js
+++ js/scripts.js
@@ -20,6 +20,27 @@ $(function() {
     var addInput2 = parseInt($("#add2").val());
     var result = add(addInput1, addInput2);
     event.preventDefault();
-    $("#output").text(result);
+    $("#output").text("Additon Result: "+result);
   });
+  $("form#subtract").submit(function(event){
+    var subInput1 = parseInt($("input#sub1").val());
+    var subInput2 = parseInt($("input#sub2").val());
+    var result = subtract(subInput1, subInput2);
+    event.preventDefault();
+    $("#output").text("Subtraction Result: "+ result);
+  });
+  $("form#multiply").submit(function(event) {
+    var multInput1 = parseInt($("#mult1").val());
+    var multInput2 = parseInt($("#mult2").val());
+    var result = multiply(multInput1, multInput2);
+    event.preventDefault();
+    $("#output").text("Multiplication Result: " + result);
+  });
+  $("form#divide").submit(function(event){
+    var divide1 = parseInt($("input#divide1").val());
+    var divide2 = parseInt($("input#divide2").val());
+    var result = divide(divide1,divide2);
+    event.preventDefault();
+    $("#output").text("Division Result: "+ result);
+  })
 });
